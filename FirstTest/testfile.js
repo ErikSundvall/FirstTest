62e9d727dc145cc315b30f0743dc532316c2c5ef
Erik Sundvall <erik.sundvall@liu.se>
2011-12-13 12:11:22 +0100

    html all of a sudden?

--- FirstTest/testfile.js
+++ FirstTest/testfile.js
@@ -1,4 +1,31 @@
-/**
- * Guess what, this is a test!
- * Now edited at github using the online web editor, and then in Eclipse again.
- */
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  	<head>
+		<meta content="text/html; charset=utf-8" http-equiv="content-type" />
+		<title>Test</title>
+		<!-- script type="application/x-javascript" src="/js/d3-2.0.0/d3.min.js" --><!-- /script -->
+		<script type="application/x-javascript" src="/js/d3-2.0.0/d3.time.js"></script>
+
+		<!-- link rel="stylesheet" href="/css/timeline.css" type="text/css" -->
+    <style type="text/css">
+		body {
+			margin: 0;  
+			padding: 0; 
+			background: white;
+		}
+		</style>
+		
+</head>
+<body>
+<div id="EEE_UI">		
+		<script type="text/javascript">
+		  var balin=true;
+		  var ainur=false;
+		  var cachedMode = true;
+		  console.log("hello world, starting timeline-2 script tag")
+		</script>
+		<div id="timeline">Testing!</div>
+		</div><!-- end of div id="EEE_UI" -->
+	</body>
+</html>
